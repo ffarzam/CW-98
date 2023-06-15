e14041479c997925b87bbdc0e81920030959f8e7
Farzam <ffarzam_1992@yahoo.com>
2023-06-15 16:48:05 +0330

    adding cw11-3-C.py

---
+++ cw11-3-C.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--date", type=str,required=True)
+parser.add_argument("--input-format", type=str,required=True)
+parser.add_argument("--output-format", type=str,required=True)
+
+args = parser.parse_args()
+date_format = datetime.strptime(args.date, args.input_format)
+string_format = date_format.strftime(args.output_format)
+
+print("\033[0;31m"f"{string_format}""\033[0m")
