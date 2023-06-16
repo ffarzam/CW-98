94c060b57c348a189638ede92ca02a200f1c5968
Farzam <ffarzam_1992@yahoo.com>
2023-06-16 16:50:31 +0330

    adding part 3 for CW11-2-2: SQL

--- week11/scratch.sql
+++ week11/scratch.sql
@@ -3,3 +3,24 @@ SELECT * FROM film WHERE rental_duration > 4
 
 --part 2
 (SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5
+
+--part 3
+
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC
+
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY rental_duration
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY rental_duration DESC
+
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY last_update
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY last_update DESC
+
+
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY title
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY title DESC
+
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY rental_duration
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY rental_duration DESC
+
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY last_update
+SELECT * FROM film WHERE rental_duration > 2 and rental_duration <5 ORDER BY last_update DESC
