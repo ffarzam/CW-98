ef672a4a6492807d0fee3ff9b2d0a946405f8356
Farzam <ffarzam_1992@yahoo.com>
2023-06-22 15:14:40 +0330

    Adding CW12-2: SQL2 Part 5

--- week12/CW12-2-SQL2.sql
+++ week12/CW12-2-SQL2.sql
@@ -22,4 +22,11 @@ INNER JOIN category ON category.category_id = film_category.category_id
 GROUP BY name
 HAVING count(*) > 60 AND count(*) < 68;
 
+--part 5
+
+SELECT language.name language, film.title, category.name category
+From film INNER JOIN language ON language.language_id = film.language_id
+INNER JOIN film_category ON film_category.film_id = film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id;
+
 
