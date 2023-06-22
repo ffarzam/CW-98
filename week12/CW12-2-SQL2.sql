69697288d3c2eb0edfc9a968dd051c3f612f4702
Farzam <ffarzam_1992@yahoo.com>
2023-06-22 15:33:45 +0330

    Adding CW12-2: SQL2 Part 6

--- week12/CW12-2-SQL2.sql
+++ week12/CW12-2-SQL2.sql
@@ -29,4 +29,11 @@ From film INNER JOIN language ON language.language_id = film.language_id
 INNER JOIN film_category ON film_category.film_id = film.film_id
 INNER JOIN category ON category.category_id = film_category.category_id;
 
+--part 6
+SELECT customer.first_name,customer.last_name, film.title,AGE(return_date,rental_date) AS rental_duration
+FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN film ON film.film_id = inventory.inventory_id;
+
 
