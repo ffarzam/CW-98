305d3647dce6b8c0be0a70418155ab2f3f07f80b
Farzam <ffarzam_1992@yahoo.com>
2023-06-22 14:51:48 +0330

    Adding CW12-2: SQL2 Part 2

--- week12/CW12-2-SQL2.sql
+++ week12/CW12-2-SQL2.sql
@@ -3,6 +3,11 @@ SELECT title, release_year, name
 From film INNER JOIN film_category on film_category.film_id = film.film_id
 INNER JOIN category ON category.category_id = film_category.category_id;
 
+--part 2
+SELECT title, release_year, name
+From film INNER JOIN film_category on film_category.film_id = film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id
+WHERE name IN ('Action', 'Comedy' , 'Family');
 
 
 
