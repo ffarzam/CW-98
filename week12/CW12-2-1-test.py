b561076e5920a5a1fddcf920bbf24abbebf2c5c9
Farzam <ffarzam_1992@yahoo.com>
2023-06-23 19:43:37 +0330

    CW12 - CW12-2 test
    Updating cw12-2-1-test

--- week12/CW12-2-1-test.py
+++ week12/CW12-2-1-test.py
@@ -4,13 +4,13 @@ response1 = rq.get("http://192.168.1.167:8080/")
 print(response1.json())
 
 response2 = rq.post("http://192.168.1.167:8080/", json={"id": 1, "title": "HOME", "description": "clean the house"})
-print(response2.text)
+print(response2.json())
 
 response3 = rq.get("http://192.168.1.167:8080/")
 print(response3.json())
 
 response4 = rq.post("http://192.168.1.167:8080/", json={"id": 2, "title": "DOGS", "description": "feed the dogs"})
-print(response4.text)
+print(response4.json())
 
 response5 = rq.get("http://192.168.1.167:8080/")
 print(response5.json())
