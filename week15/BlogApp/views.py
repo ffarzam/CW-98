768869f26232e77cc457b0e99040785d56722bbb
Farzam <ffarzam_1992@yahoo.com>
2023-07-13 22:47:08 +0330

    Updating views.py

--- week15/BlogApp/views.py
+++ week15/BlogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -10,10 +10,20 @@ def home(request):
 
 
 def post_list(request):
-    all_posts = Post.objects.all
+    all_posts = Post.objects.all()
     return render(request, "post_list.html", {"all_posts": all_posts})
 
 
 def post_details(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, "post_details.html", {"post": post})
+
+
+def category_list(request):
+    all_category = Category.objects.all()
+    return render(request, "category_list.html", {"all_category": all_category})
+
+
+def categories_details(request, pk):
+    category = Category.objects.get(id=pk)
+    return render(request, "category_details.html", {"category": category})
