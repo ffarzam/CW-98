b356b40974503dee9e88c2bb3d23f97e34aeb0fa
Farzam <ffarzam_1992@yahoo.com>
2023-07-13 16:24:31 +0330

    Adding models.py

--- week15/BlogApp/models.py
+++ week15/BlogApp/models.py
@@ -7,6 +7,9 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.CharField(max_length=500)
 
+    def __str__(self) -> str:
+        return f"{self.name}: {self.bio}"
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -14,3 +17,23 @@ class Post(models.Model):
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
 
+    def __str__(self) -> str:
+        return f"{self.title}: {self.author}, {self.publication_date}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100),
+    description = models.TextField(),
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.description}"
+
+
+class Comment(models.Model):
+    post = models.CharField(max_length=100),
+    author = models.CharField(max_length=50),
+    content = models.TextField(),
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.post}:{self.author}({self.date})"
