c2fd1fd35712a929c6814150119c480f1a35dad9
Farzam <ffarzam_1992@yahoo.com>
2023-07-13 19:27:05 +0330

    Adding post_list view

--- week15/BlogApp/views.py
+++ week15/BlogApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "home.html")
 
 
+def post_list(request):
+    all_posts = Post.objects.all
+    return render(request, "post_list.html", {"all_posts": all_posts})
+
+# def posts_details(request,)
