9f317b59d865b858322dfe33dcec1a2c6a99c5ff
Farzam <ffarzam_1992@yahoo.com>
2023-07-13 22:46:44 +0330

    Updating urls.py

--- week15/BlogApp/urls.py
+++ week15/BlogApp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('post/', views.post_list, name="post_list"),
     path('posts/<int:pk>/', views.post_details, name="post_details"),
-    # path('categories/', views.category_list),
-    # path('categories/<int:pk>/', views.categories_details),
+    path('categories/', views.category_list, name="category_list"),
+    path('categories/<int:pk>/', views.categories_details, name="categories_details"),
     # path('authors/', views.author_list),
     # path('authors/<int:pk>/', views.authors_details),
 
