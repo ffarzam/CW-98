b1157bdc27952f30c2514ed815d1ce233152382b
Farzam <ffarzam_1992@yahoo.com>
2023-08-03 18:10:52 +0330

    fix: register url fixed

--- week16/TaskManagement/users/urls.py
+++ week16/TaskManagement/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name="login"),
-    path('', views.register, name="register"),
+    path('register/', views.register, name="register"),
 ]
