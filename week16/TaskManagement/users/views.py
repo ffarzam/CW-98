71b23f93efc5e3baa9a1243fc7b57d6bb5da0635
Farzam <ffarzam_1992@yahoo.com>
2023-08-03 20:25:20 +0330

    chore: authentication added to login view

--- week16/TaskManagement/users/views.py
+++ week16/TaskManagement/users/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate
 from django.contrib.auth import login, logout
+from .authentication import MyAuthBackend
 
 # Create your views here.
 
@@ -29,20 +28,20 @@ def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            print("1"*100)
-            user = authenticate(
+            user = MyAuthBackend().authenticate(
+                request,
                 username=form.cleaned_data["username_or_email"],
                 password=form.cleaned_data["password"])
             if user is None:
-                print("2" * 100)
-                user = authenticate(
+                user = MyAuthBackend().authenticate(
+                    request,
                     email=form.cleaned_data["username_or_email"],
                     password=form.cleaned_data["password"])
                 if user is None:
-                    print("3" * 100)
                     message = "user or passwords is invalid"
                     return render(request, 'register.html', {'form': form, "message": message})
-            login(request, user)
+
+            login(request, user, backend='users.authentication.MyAuthBackend')
             return redirect("home")
 
     else:
