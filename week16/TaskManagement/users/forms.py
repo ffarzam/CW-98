0103bc527cbfa4be21ea8f67241dae672f4ffe73
Farzam <ffarzam_1992@yahoo.com>
2023-08-03 23:17:00 +0330

    refactor: add password to meta

--- week16/TaskManagement/users/forms.py
+++ week16/TaskManagement/users/forms.py
@@ -3,12 +3,12 @@ from .models import CustomUser
 
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', ]
+        fields = ['username', 'email', 'password']
+        widgets = {"password": forms.PasswordInput}
 
 
 class LoginForm(forms.ModelForm):
