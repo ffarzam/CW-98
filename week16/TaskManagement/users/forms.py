1ef29219ecbd733db98bab4162504f966e8aa200
Farzam <ffarzam_1992@yahoo.com>
2023-08-03 18:30:46 +0330

    refactor: change register form

--- week16/TaskManagement/users/forms.py
+++ week16/TaskManagement/users/forms.py
@@ -3,8 +3,9 @@ from .models import CustomUser
 
 
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email',]
