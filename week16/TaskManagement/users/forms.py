eef39d5b9f28587f75727848f8ea1db96f4d0573
Farzam <ffarzam_1992@yahoo.com>
2023-08-03 18:05:40 +0330

    chore: register form created

--- week16/TaskManagement/users/forms.py
+++ week16/TaskManagement/users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import CustomUser
 
 
-class CommentForm(forms.ModelForm):
+class RegisterForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
