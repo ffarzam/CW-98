d4cdea7304b3b7c626c58744d16f58e0c903c4b2
Farzam <ffarzam_1992@yahoo.com>
2023-08-08 19:52:42 +0330

    chore: permissiondenied url added

--- week19/Todo Project/Home/urls.py
+++ week19/Todo Project/Home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('todos/', views.TodoListView.as_view(), name='todo_list'),
     path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
     path('thank_you/', views.thank_you, name='thank_you'),
+    path('permissiondenied/', views.permission_denied, name='permissiondenied'),
 ]
