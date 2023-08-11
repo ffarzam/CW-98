108d1078e236c46473c8a9cceaea807f1d2f9b04
Farzam <ffarzam_1992@yahoo.com>
2023-08-11 17:07:12 +0330

    chore: add sixth exercise js

---
+++ week19-front/week19-6.js
@@ -0,0 +1,9 @@
+const Info = {
+    firstName: "Farzam",
+    lastName: "Pil Aghaei",
+    age: 31,
+    country: "Iran",
+    city: "Langroud"
+}
+
+window.localStorage.setItem('Info', JSON.stringify(Info));
