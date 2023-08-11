8270225d7e9897ba8439961bc099b874e5e67ab2
Farzam <ffarzam_1992@yahoo.com>
2023-08-11 15:56:05 +0330

    refactor: add addEventlistener

--- week19-front/week19-4.js
+++ week19-front/week19-4.js
@@ -1,3 +1,15 @@
+let p = document.getElementById("demo")
+
+document.getElementById("btn").addEventListener("click", myFunction)
+
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+
+  if(p.style.display == "none"){
+
+  p.style.display = "block";
+  }
+
+  else {
+  p.style.display = "none";
+  }
 }
