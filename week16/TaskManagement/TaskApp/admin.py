f9a1e7066e41c595cc201ca4d8c5974bc9607613
Farzam <ffarzam_1992@yahoo.com>
2023-08-18 11:54:37 +0330

    chore: add export to csv

--- week16/TaskManagement/TaskApp/admin.py
+++ week16/TaskManagement/TaskApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category, Task, Tag
+import csv
+from django.http import HttpResponse
 
 # Register your models here.
 
@@ -10,6 +12,22 @@ admin.site.register(Tag)
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["title", "is_active","created","updated" ]
+    list_display = ["title", "is_active", "created", "updated"]
     readonly_fields = ["is_active"]
+    actions = ["export_as_csv"]
 
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
