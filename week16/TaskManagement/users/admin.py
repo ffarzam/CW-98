c17afc1dca93d1bd7b6d0c6e0603049969f093fe
Farzam <ffarzam_1992@yahoo.com>
2023-08-18 10:23:25 +0330

    chore: add number of task

--- week16/TaskManagement/users/admin.py
+++ week16/TaskManagement/users/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import CustomUser
 from django.contrib.sessions.models import Session
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from django.db.models import Count
+
 
 # Register your models here.
 
-admin.site.register(CustomUser)
+# admin.site.register(CustomUser)
 
 
 class SessionAdmin(admin.ModelAdmin):
@@ -15,4 +19,29 @@ class SessionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Session, SessionAdmin)
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "task_count"]
+    # ordering = ["number_of_tasks"]
+
+    # @admin.display(ordering='task_count')
+    def task_count(self, user):
+        url = (reverse('admin:TaskApp_task_changelist')
+               + '?'
+               + urlencode({
+                    'user__id': str(user.id)
+                }))
+
+        return format_html('<a href="{}">{}</a>', url, user.task_count)
+
+    task_count.short_description = 'Number Of Tasks'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            task_count=Count('task__user')
+        )
+
+
 
