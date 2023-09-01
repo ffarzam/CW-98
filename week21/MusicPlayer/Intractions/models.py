50429a41e311b00be846fed532d32530cc0dbb24
Farzam <ffarzam_1992@yahoo.com>
2023-09-01 18:33:56 +0330

    chore: add intractions app models

--- week21/MusicPlayer/Intractions/models.py
+++ week21/MusicPlayer/Intractions/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from Song.models import Song
+from accounts.models import User
+
 
 # Create your models here.
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} liked on {self.song.title}"
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    is_confirmed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} commented on {self.song.title}"
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.title
