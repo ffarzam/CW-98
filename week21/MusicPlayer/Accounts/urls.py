4b6c50051e433e14785765b263e0367076f2f1a2
Farzam <ffarzam_1992@yahoo.com>
2023-09-03 07:08:58 +0330

    chore: add accounts app urls

---
+++ week21/MusicPlayer/Accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.Login.as_view(), name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout_view, name="logout"),
+
+]
