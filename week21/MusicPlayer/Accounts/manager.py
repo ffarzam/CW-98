dd78982d27b950a081320e16109d829e30f19686
Farzam <ffarzam_1992@yahoo.com>
2023-09-03 07:07:29 +0330

    chore: add custom manager

--- week21/MusicPlayer/Accounts/manager.py
+++ week21/MusicPlayer/Accounts/manager.py
@@ -2,17 +2,19 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class CustomManager(BaseUserManager):
-    def create_user(self, email, password):
+    def create_user(self, email, username, password):
         if not email:
             raise ValueError("Users must have an email")
+        if not username:
+            raise ValueError("Users must have an username")
 
         user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, username, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
