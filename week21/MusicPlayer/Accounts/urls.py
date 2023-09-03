1e049fdc51302900a9a39936ad6c200c320693d1
Farzam <ffarzam_1992@yahoo.com>
2023-09-03 07:22:57 +0330

    chore: change logout view to class view

--- week21/MusicPlayer/Accounts/urls.py
+++ week21/MusicPlayer/Accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.Login.as_view(), name="login"),
     path('register/', views.register, name="register"),
-    path('logout/', views.logout_view, name="logout"),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
 
 ]
