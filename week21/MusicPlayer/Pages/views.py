c53785ad3df0c3e2760b350cdd031d6eb83a5971
Farzam <ffarzam_1992@yahoo.com>
2023-09-03 07:30:54 +0330

    chore: add homepage view

--- week21/MusicPlayer/Pages/views.py
+++ week21/MusicPlayer/Pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from Songs.models import Song
+
 
 # Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        all_songs = Song.objects.all()
+
+        context = {"tasks": all_songs}
+        return render(request, "home/home.html", context=context)
