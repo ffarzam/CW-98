6844b63bc976f01187bb563b8948b2f0c74f77f7
Farzam <ffarzam_1992@yahoo.com>
2023-09-05 07:46:23 +0330

    chore: update songs model

--- week21/MusicPlayer/Songs/models.py
+++ week21/MusicPlayer/Songs/models.py
@@ -13,11 +13,28 @@ class Genre(models.Model):
 
 class Song(models.Model):
     title = models.CharField(max_length=50)
-    audio_file = models.FileField(upload_to="AudioFile/% Y/% m/% d/")
-    cover_photo = models.ImageField(upload_to="Image% Y/% m/% d/")
+    audio_file = models.FileField(upload_to="AudioFile/%Y/%m/%d/")
+    cover_photo = models.ImageField(upload_to="Image/%Y/%m/%d/")
     upload_date = models.DateField(auto_now_add=True, editable=False)
     genres = models.ManyToManyField(Genre)
     band = models.ForeignKey("Accounts.Band", on_delete=models.PROTECT, null=True, blank=True)
+
+    @property
+    def song_artists(self):
+        # songs = Song.objects.prefetch_related("artist_set").annotate(name=F("artist__name")).filter(id=self.id)
+        artists = self.artist_set.all()
+        return list(artists)
+
+    @property
+    def song_genres(self):
+        songs = self.genres.all()
+        return list(songs)
+
+    @property
+    def number_of_likes(self):
+        # qs = Like.objects.select_related("song").filter(song=self.id).aggregate(count=Count("song"))
+        likes = self.like_set.all()
+        return len(likes)
 
     def __str__(self):
         return self.title
