27d5a10820512c23e5d18f6838b19b5d80823248
Farzam <ffarzam_1992@yahoo.com>
2023-09-06 23:25:28 +0330

    chore: update pages urls

--- week21/MusicPlayer/Pages/urls.py
+++ week21/MusicPlayer/Pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name="home"),
     path('', views.Home.as_view(), name="home"),
+    path('profile/<int:id>', views.profile, name="profile"),
+    path('profile_edit/', views.profile_edit, name="profile_edit"),
 ]
